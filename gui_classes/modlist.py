1847706e8ee04c36baff160113f7b1ce42b2b864
Caleb <caleb.harmon2345@gmail.com>
2023-03-17 01:12:11 +0000

    Ability to switch to compact modlist

--- gui_classes/modlist.py
+++ gui_classes/modlist.py
@@ -3,12 +3,17 @@ import api.spacedock_api as sdapi
 
 
 class AvailableModMenu(customtkinter.CTkFrame):
+
     def __init__(self, master, **kwargs):
         super().__init__(master)
         self.modlist_frame = kwargs.get("modlist_frame", None)
         self.header_text_frame = kwargs.get("header_text_frame", None)
 
-        self.available_mods_menu = customtkinter.CTkOptionMenu(self, values=["All", "Featured", "Top", "New"], command=self.optionmenu_callback, width=100)
+        self.available_mods_menu = customtkinter.CTkOptionMenu(
+            self,
+            values=["All", "Featured", "Top", "New"],
+            command=self.optionmenu_callback,
+            width=100)
         self.available_mods_menu.grid(row=0, column=0)
         self.available_mods_menu.set("All")
 
@@ -23,9 +28,9 @@ class AvailableModMenu(customtkinter.CTkFrame):
 
     def get_available_mods_category(self, category):
         print("Fetching available mods")
-        self.modlist_frame.clear_modlist()   
-        self.modlist_frame.loading_screen_frame.show() 
-        t = threading.Thread(target=self.fetch_mods, args=(category,))
+        self.modlist_frame.clear_modlist()
+        self.modlist_frame.loading_screen_frame.show()
+        t = threading.Thread(target=self.fetch_mods, args=(category, ))
         t.start()
 
     def optionmenu_callback(self, choice):
@@ -51,19 +56,44 @@ class AvailableModMenu(customtkinter.CTkFrame):
         self.header_text_frame.configure(text=text)
 
 
-        
 class ModListHeaderFrame(customtkinter.CTkFrame):
+
     def __init__(self, master, **kwargs):
         super().__init__(master)
 
         self.grid_columnconfigure(0, weight=1)
         self.modlist_frame = kwargs.get("modlist_frame", None)
 
-        self.header_text_frame = customtkinter.CTkLabel(self, text="", font=customtkinter.CTkFont(size=18, weight="bold"))
-        self.header_text_frame.grid(row=0, column=0, sticky="w", padx=10, pady=10)
+        self.header_text_frame = customtkinter.CTkLabel(
+            self, text="", font=customtkinter.CTkFont(size=18, weight="bold"))
+        self.header_text_frame.grid(row=0,
+                                    column=0,
+                                    sticky="w",
+                                    padx=10,
+                                    pady=10)
 
-        self.available_mods_menu = AvailableModMenu(self, modlist_frame=self.modlist_frame, header_text_frame=self.header_text_frame)
-        self.available_mods_menu.grid(row=0, column=1, sticky="e", padx=10, pady=10)
+        self.available_mods_menu = AvailableModMenu(
+            self,
+            modlist_frame=self.modlist_frame,
+            header_text_frame=self.header_text_frame)
+        self.available_mods_menu.grid(row=0,
+                                      column=1,
+                                      sticky="e",
+                                      padx=10,
+                                      pady=10)
+
+        self.switch_var = customtkinter.StringVar(value="off")
+        self.compact_switch = customtkinter.CTkSwitch(
+            self,
+            text="Compact",
+            command=self.set_compact_modlist,
+            variable=self.switch_var,
+            onvalue="on",
+            offvalue="off")
+        self.compact_switch.grid(row=0, column=2, sticky="e", padx=10, pady=10)
+
+    def set_compact_modlist(self):
+        self.modlist_frame.toggle_compact()
 
     def set_header_text(self, text):
         self.header_text_frame.configure(text=text)
@@ -74,14 +104,21 @@ class ModListHeaderFrame(customtkinter.CTkFrame):
         if val:
             self.available_mods_menu.grid_remove()
         else:
-            self.available_mods_menu.grid(row=0, column=1, sticky="e", padx=10, pady=10)
+            self.available_mods_menu.grid(row=0,
+                                          column=1,
+                                          sticky="e",
+                                          padx=10,
+                                          pady=10)
 
-        
 
 class LoadingScreenFrame(customtkinter.CTkFrame):
+
     def __init__(self, master):
         super().__init__(master)
-        self.loading_label = customtkinter.CTkLabel(self, text="Retrieving mod data from spacedock.info...", font=customtkinter.CTkFont(size=12, weight="bold"))
+        self.loading_label = customtkinter.CTkLabel(
+            self,
+            text="Retrieving mod data from spacedock.info...",
+            font=customtkinter.CTkFont(size=12, weight="bold"))
         self.loading_label.pack(pady=30)
 
     def show(self):
@@ -91,8 +128,8 @@ class LoadingScreenFrame(customtkinter.CTkFrame):
         self.grid_remove()
 
 
-
 class ModListFrame(customtkinter.CTkScrollableFrame):
+
     def __init__(self, master, **kwargs):
         super().__init__(master)
 
@@ -106,39 +143,86 @@ class ModListFrame(customtkinter.CTkScrollableFrame):
     def add_item(self, item):
         """Adds a mod item to the mod list"""
 
-        row_frame = customtkinter.CTkFrame(self, fg_color="lightgray" if customtkinter.get_appearance_mode() == "Light" else "black", cursor="hand2")
-        
-        name_label = customtkinter.CTkLabel(row_frame, text=item.name, font=customtkinter.CTkFont(size=14, weight="bold"), bg_color="lightblue" if customtkinter.get_appearance_mode() == "Light" else "gray9", padx=10, anchor="w")
+        row_frame = customtkinter.CTkFrame(
+            self,
+            fg_color="lightgray"
+            if customtkinter.get_appearance_mode() == "Light" else "black",
+            cursor="hand2")
+
+        name_label = customtkinter.CTkLabel(
+            row_frame,
+            text=item.name,
+            font=customtkinter.CTkFont(size=14, weight="bold"),
+            bg_color="lightblue"
+            if customtkinter.get_appearance_mode() == "Light" else "gray9",
+            padx=10,
+            anchor="w")
         name_label.grid(row=0, column=0, pady=(10, 0), padx=10, sticky="w")
-        
-        author_label = customtkinter.CTkLabel(row_frame, text=f"by {item.author}", font=customtkinter.CTkFont(size=12), padx=5)
+
+        author_label = customtkinter.CTkLabel(
+            row_frame,
+            text=f"by {item.author}",
+            font=customtkinter.CTkFont(size=12),
+            padx=5)
         author_label.grid(row=1, column=0, padx=10, sticky="w")
-        
-        summary_label = customtkinter.CTkLabel(row_frame, text=item.short_description[:50] + '...' if len(item.short_description) > 50 else item.short_description, font=customtkinter.CTkFont(size=12), padx=5, anchor="w")
+
+        summary_label = customtkinter.CTkLabel(
+            row_frame,
+            text=item.short_description[:50] + '...'
+            if len(item.short_description) > 50 else item.short_description,
+            font=customtkinter.CTkFont(size=12),
+            padx=5,
+            anchor="w")
         summary_label.grid(row=2, column=0, pady=(0, 10), padx=10, sticky="w")
-        
-        game_version_label = customtkinter.CTkLabel(row_frame, text=f"Game Version: {item.game_version}", font=customtkinter.CTkFont(size=10), padx=10)
+
+        game_version_label = customtkinter.CTkLabel(
+            row_frame,
+            text=f"Game Version: {item.game_version}",
+            font=customtkinter.CTkFont(size=10),
+            padx=10)
         game_version_label.grid(row=2, column=4, padx=10, sticky="w")
 
-        downloads_label = customtkinter.CTkLabel(row_frame, text=f"\u2193 {item.downloads}", font=customtkinter.CTkFont(size=12), padx=10)
+        downloads_label = customtkinter.CTkLabel(
+            row_frame,
+            text=f"\u2193 {item.downloads}",
+            font=customtkinter.CTkFont(size=12),
+            padx=10)
         downloads_label.grid(row=1, column=4, padx=10, sticky="e")
 
-        installed_label = customtkinter.CTkLabel(row_frame, text="Installed" if item.installed else "", font=customtkinter.CTkFont(size=12), text_color="green", padx=10)
+        installed_label = customtkinter.CTkLabel(
+            row_frame,
+            text="Installed" if item.installed else "",
+            font=customtkinter.CTkFont(size=12),
+            text_color="green",
+            padx=10)
         installed_label.grid(row=0, column=4, padx=10, sticky="e")
-        
+
         row_frame.grid(sticky="ew", padx=5, pady=5)
         row_frame.columnconfigure(2, weight=1)
 
         row_frame.columnconfigure(4, minsize=70)
-        self.item_widgets.append((row_frame, name_label, author_label, summary_label, game_version_label, installed_label))
+        self.item_widgets.append(
+            (row_frame, name_label, author_label, summary_label,
+             game_version_label, downloads_label, installed_label))
 
         # Bind mod_select_event to button click event
         row_frame.bind("<Button-1>", lambda event: self.modlist_select(item))
 
         # On row_frame hover, do something
-        row_frame.bind("<Enter>", lambda event, frame=row_frame: self.on_frame_hover(event, frame, name_label))
-        row_frame.bind("<Leave>", lambda event, frame=row_frame: self.on_frame_leave(event, frame, name_label))
+        row_frame.bind("<Enter>",
+                       lambda event, frame=row_frame: self.on_frame_hover(
+                           event, frame, name_label))
+        row_frame.bind("<Leave>",
+                       lambda event, frame=row_frame: self.on_frame_leave(
+                           event, frame, name_label))
 
+    def toggle_compact(self):
+        for item in self.item_widgets:
+            item[1].grid(row=0, column=0, pady=(0, 0), padx=0, sticky="w")
+            item[2].grid(row=0, column=3, padx=10, sticky="e")
+            item[3].grid_remove()
+            item[4].grid_remove()
+            item[5].grid_remove()
 
     def clear_modlist(self):
         """Clears the mod list"""
@@ -148,8 +232,8 @@ class ModListFrame(customtkinter.CTkScrollableFrame):
             widget_set[0].destroy()
         self.item_widgets.clear()
 
-    def populate_modlist(self, mods):     
-        """Populates the mod list with the given mods"""   
+    def populate_modlist(self, mods):
+        """Populates the mod list with the given mods"""
         self.clear_modlist()
 
         for i in range(len(mods)):
@@ -162,17 +246,25 @@ class ModListFrame(customtkinter.CTkScrollableFrame):
         """Updates the appearance of the mod list based on darkmode or lightmode selection"""
 
         for widget_set in self.item_widgets:
-            widget_set[0].configure(fg_color="lightgray" if customtkinter.get_appearance_mode() == "Light" else "black")
-            widget_set[1].configure(bg_color="lightblue" if customtkinter.get_appearance_mode() == "Light" else "gray9")
-        
+            widget_set[0].configure(
+                fg_color="lightgray" if customtkinter.get_appearance_mode() ==
+                "Light" else "black")
+            widget_set[1].configure(
+                bg_color="lightblue" if customtkinter.get_appearance_mode() ==
+                "Light" else "gray9")
+
     def on_frame_hover(self, event, frame, name_label):
-        frame.configure(fg_color="gray" if customtkinter.get_appearance_mode() == "Light" else "gray8")
-        name_label.configure(bg_color="lightblue" if customtkinter.get_appearance_mode() == "Light" else "gray9")
+        frame.configure(fg_color="gray" if customtkinter.get_appearance_mode(
+        ) == "Light" else "gray8")
+        name_label.configure(bg_color="lightblue" if customtkinter.
+                             get_appearance_mode() == "Light" else "gray9")
 
     def on_frame_leave(self, event, frame, name_label):
-        frame.configure(fg_color="lightgray" if customtkinter.get_appearance_mode() == "Light" else "black")
-        name_label.configure(bg_color="lightblue" if customtkinter.get_appearance_mode() == "Light" else "gray9")
-    
+        frame.configure(fg_color="lightgray" if customtkinter.
+                        get_appearance_mode() == "Light" else "black")
+        name_label.configure(bg_color="lightblue" if customtkinter.
+                             get_appearance_mode() == "Light" else "gray9")
+
     def modlist_select(self, item):
         """Event handler for when a mod is selected in the mod list.
         loads the mod's info into the control panel"""
